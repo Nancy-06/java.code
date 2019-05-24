d7b95d4829eefe99b519b637e32e138eb2d97de5
Nancy-06 <1456387238@qq.com>
2019-05-24 20:43:37 +0800

    提交代码

---
+++ Practice1.java
@@ -0,0 +1,9 @@
+public class Practice1{
+	public static void main(String [ ] args){
+	  System.out.println("你好中国");
+	  for( int i = 0;i<10;i++ ){
+		  //字符串+运算就是字符串的拼接
+	  System.out.println( "Hello" +i );
+	  }
+	}
+}
