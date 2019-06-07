02eee0c0e5a541ede84a8b002338417d42d8cf9d
Nancy-06 <1456387238@qq.com>
2019-06-07 23:38:34 +0800

    提交代码

---
+++ Practice14.java
@@ -0,0 +1,10 @@
+public class Practice14{
+	public static void main(String [] args){
+		int value1=4;
+		int value2=8;
+		int result;
+		boolean condition=true;
+		result=condition ? value1 :value2 ;
+		System.out.println(result);
+	}
+}
